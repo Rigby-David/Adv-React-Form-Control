1101180c6e484ca798261c0a42cd3e62f1093d3a
David M Rigby <RIGBY335@HOTMAIL.COM>
2022-09-27 17:18:44 -0700

    Initial commit

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import styles from './Projects.css';
+
+export default function Projects() {
+  return (
+    <div className={styles.Projects}>
+      <h1>Projects</h1>
+      
+    </div>
+
+  );
+}
