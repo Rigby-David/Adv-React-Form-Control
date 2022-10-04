0fcd43d55692cc38136bac02a5acdd7caa7355bd
David <Rigby335@hotmail.com>
2022-10-04 09:25:24 -0700

    implemented InputControl in SearchForm which is called in Search to render search input to page

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,12 +1,24 @@
-import { Link } from 'react-router-dom';
+import useSearchResults from '../Hooks/use-search-results';
+import { FormButton } from '../Projects/Forms/FormControl';
+import SearchForm from './SearchForm.jsx';
 
 export default function Search() {
+  const {
+    infiniteScrollRef,
+    nextPage,
+    pokemon,
+    setPokemon,
+    searchResults,
+    searchPokedex,
+  } = useSearchResults();
   return (
-    <div>
-      <h1>Search</h1>
-      <nav>
-        <Link to=""></Link>
-      </nav>
-    </div>
+    <section>
+      <SearchForm
+        pokemon={pokemon}
+        setPokemon={setPokemon}
+        onsubmit={searchPokedex}
+      />
+      <FormButton/>
+    </section>
   );
 }
---
+++ src/components/Search/SearchForm.jsx
@@ -0,0 +1,25 @@
+import useSearchForm from '../Hooks/use-search-form';
+import {
+  InputControl
+} from '../Projects/Forms/FormControl.jsx';
+
+export default function SearchForm({ onsubmit }) {
+  const { pokemon, setPokemon, type_1, setType_1 } = useSearchForm();
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formDataObject = Object.fromEntries(formData);
+    onsubmit(formDataObject);
+  };
+
+  return (
+    <InputControl
+      label="Search by Poke'mon name"
+      type="text"
+      name="pokemon"
+      value={pokemon}
+      onChange={(e) => setPokemon(e.target.value)}
+    />
+  );
+}
